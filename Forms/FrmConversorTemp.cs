2404ea80afa1bbe54d89202692a616dd9c008b74
Juan <jpcuomo2000@gmail.com>
2024-02-08 15:37:01 -0300

    Commit 4 (Refactorización de  metodos TextChanged)

--- Forms/FrmConversorTemp.cs
+++ Forms/FrmConversorTemp.cs
@@ -30,23 +30,17 @@ namespace Forms
         {
             double temperatura;
 
-            // Conversion de Fahrenheit a Celsius
-            if (ValidarTemperatura(tbxFahrenheit, tbxFahrenheitACelsius, out temperatura))
+            if (ValidarTemperatura(tbxFahrenheit, tbxFahrenheitACelsius, tbxFahrenheitAFahrenheit, tbxFahrenheitAKelvin, out temperatura))
             {
+                // Conversion de Fahrenheit a Celsius
                 f = new Fahrenheit(temperatura);
                 c = (Celsius)f;
                 tbxFahrenheitACelsius.Text = c.Temperatura.ToString("F4");
-            }
 
-            // Conversión de Fahrenheit a Fahrenheit
-            if (ValidarTemperatura(tbxFahrenheit, tbxFahrenheitAFahrenheit, out temperatura))
-            {
+                // Conversión de Fahrenheit a Fahrenheit
                 tbxFahrenheitAFahrenheit.Text = f.Temperatura.ToString("F4");
-            }
 
-            // Conversión de Fahrenheit a Kelvin
-            if (ValidarTemperatura(tbxFahrenheit, tbxFahrenheitAKelvin, out temperatura))
-            {
+                // Conversión de Fahrenheit a Kelvin
                 k = (Kelvin)f;
                 tbxFahrenheitAKelvin.Text = k.Temperatura.ToString("F4");
             }
@@ -57,23 +51,17 @@ namespace Forms
         {
             double temperatura;
 
-            // Conversion de Celsius a Fahrenheit
-            if (ValidarTemperatura(tbxCelsius, tbxCelsiusAFahrenheit, out temperatura))
+            if (ValidarTemperatura(tbxCelsius, tbxCelsiusAFahrenheit, tbxCelsiusACelsius, tbxCelsiusAKelvin, out temperatura))
             {
+                // Conversion de Celsius a Fahrenheit
                 c = new Celsius(temperatura);
                 f = (Fahrenheit)c;
                 tbxCelsiusAFahrenheit.Text = f.Temperatura.ToString("F4");
-            }
 
-            // Conversion de Celsius a Celsius
-            if (ValidarTemperatura(tbxCelsius, tbxCelsiusACelsius, out temperatura))
-            {
+                // Conversion de Celsius a Celsius
                 tbxCelsiusACelsius.Text = c.Temperatura.ToString("F4");
-            }
 
-            // Conversion de Celsius a Kelvin
-            if (ValidarTemperatura(tbxCelsius, tbxCelsiusAKelvin, out temperatura))
-            {
+                // Conversion de Celsius a Kelvin
                 k = (Kelvin)c;
                 tbxCelsiusAKelvin.Text = k.Temperatura.ToString("F4");
             }
@@ -83,23 +71,17 @@ namespace Forms
         {
             double temperatura;
 
-            // Conversion de Kelvin a Fahrenheit
-            if (ValidarTemperatura(tbxKelvin, tbxKelvinAFahrenheit, out temperatura))
+            if (ValidarTemperatura(tbxKelvin, tbxKelvinAFahrenheit, tbxKelvinAKelvin, tbxKelvinACelsius, out temperatura))
             {
+                // Conversion de Kelvin a Fahrenheit
                 k = new Kelvin(temperatura);
                 f = (Fahrenheit)k;
                 tbxKelvinAFahrenheit.Text = f.Temperatura.ToString("F4");
-            }
 
-            // Conversion de Kelvin a Kelvin
-            if (ValidarTemperatura(tbxKelvin, tbxKelvinAKelvin, out temperatura))
-            {
+                // Conversion de Kelvin a Kelvin
                 tbxKelvinAKelvin.Text = k.Temperatura.ToString("F4");
-            }
-
-            // Conversion de Kelvin a Celsius
-            if (ValidarTemperatura(tbxKelvin, tbxKelvinACelsius, out temperatura))
-            {
+            
+                // Conversion de Kelvin a Celsius
                 c = (Celsius)k;
                 tbxKelvinACelsius.Text = c.Temperatura.ToString("F4");
             }
@@ -107,11 +89,13 @@ namespace Forms
 
 
         // Valida el valor de temperatura ingresado y maneja el comportamiento de los textBoxes
-        public static bool ValidarTemperatura(TextBox temp, TextBox tbxTempATemp, out double temperatura)
+        public static bool ValidarTemperatura(TextBox temp, TextBox tbxTempATemp1, TextBox tbxTempATemp2, TextBox tbxTempATemp3, out double temperatura)
         {
             if (string.IsNullOrEmpty(temp.Text))
             {
-                tbxTempATemp.Clear();
+                tbxTempATemp1.Clear();
+                tbxTempATemp2.Clear();
+                tbxTempATemp3.Clear();
                 temp.BackColor = Color.White;
                 temp.Clear();
             }
@@ -124,7 +108,9 @@ namespace Forms
             else
             {
                 temp.BackColor = Color.OrangeRed;
-                tbxTempATemp.Text = "Error";
+                tbxTempATemp1.Text = "Error";
+                tbxTempATemp2.Text = "Error";
+                tbxTempATemp3.Text = "Error";
             }
             temperatura = 0;
             return false;
